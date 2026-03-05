603caf6393b7cf4fc80f9b9a2fc9bf2cb830f055
crespo03-netizen <crespo.03@icloud.com>
2026-03-04 22:08:05 -0600

    A route on web was added.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate --force');
+
+    return 'Migrations completed successfully.';
 });
 
 require __DIR__.'/auth.php';
